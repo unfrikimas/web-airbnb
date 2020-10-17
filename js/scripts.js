52161176d522e1023ca49f6fae41d6825cede366
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-10-17 21:00:58 +0200

    Hero images effect with JS

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,33 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Movimiento imagen Hero
+    const imagenHero = document.querySelector('.hero');
+
+    let i = 0;
+    let tiempo = 0;
+    const imagenes = ['arriba2.jpg', 'arriba.jpg'];
+
+    setInterval(() => {
+        imagenHero.style.backgroundPositionY = '-'+tiempo+'px';
+
+        if(tiempo > 40) {
+            tiempo = 0;
+            imagenHero.style.transition = "all .3s ease-in-out";
+            imagenHero.style.backgroundImage = "url(img/"+imagenes[i]+")";
+
+            if(i === imagenes.length - 1) {
+                i = 0;
+            } else {
+                i++
+            }
+        }
+        tiempo++;
+
+    }, 100);
+
+
+    // Footer flotante
     const btnFlotante = document.querySelector('.btn-flotante');
     btnFlotante.addEventListener('click', e => {
         e.preventDefault();
